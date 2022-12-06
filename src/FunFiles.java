9c06a5de7bed297a4e682a7199d3bbc433033cf8
LivNTI <liv.sundman@ga.ntig.se>
2022-12-06 14:52:23 +0100

    End of lesson

--- src/FunFiles.java
+++ src/FunFiles.java
@@ -1,2 +1,52 @@
-package PACKAGE_NAME;public class FunFiles {
+/*
+* A class for dealing with files
+*
+* @author LivNTI
+ */
+import java.io.File;
+import java.io.IOException;
+
+public class FunFiles {
+    //attribut
+
+    //Constructor
+    public FunFiles(){
+        //findFiles();
+        createFile();
+    }
+
+    //methods
+
+    /*
+    * Method for finding all files in a specified path
+     */
+    public void findFiles(){
+            File myPath= new File("C:\\Users\\liv.sundman\\Downloads");
+            String[] allFiles= myPath.list();
+
+            //Cycles througha list of fiel and folders to print them
+            for(String fileNames: allFiles){
+                System.out.println(fileNames);
+            }
+    }
+
+    /*
+    * Creata a new file if the file does not already exist
+     */
+    public void createFile(){
+        File myFile= new File("test.txt");
+        try {
+            if(myFile.createNewFile()){
+                System.out.println("file has been created");
+            }else{
+                System.out.println("file exsits");
+            }
+        } catch(IOException e){
+            System.out.println("No can do mate!");
+            e.printStackTrace();
+        }
+    }
+
+
+
 }
